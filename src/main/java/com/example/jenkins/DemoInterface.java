0116461336c12a175b5fed78c59679d44afeaa01
FrostingMonkey <756686879@qq.com>
2020-08-30 15:18:14 +0800

    '测试'

---
+++ src/main/java/com/example/jenkins/DemoInterface.java
@@ -0,0 +1,7 @@
+package com.example.jenkins;
+
+public class DemoInterface {
+    public int  add(int a,int b){
+        return a+b;
+    }
+}
--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsApplicationTests {
 
+    @Autowired
+    private DemoInterface demoInterface;
     @Test
     void contextLoads() {
+        int add = demoInterface.add(5, 4);
+        System.out.println(add);
     }
 
 }
